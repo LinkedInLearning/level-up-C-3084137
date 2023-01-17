f952d8f6bdf5ec16fb052d7b21bde08dc68fec63
kevinpichay <120142073+kevinpichay@users.noreply.github.com>
2023-01-17 02:34:35 +0000

    01_12 done

--- src/01_12/commas_challenge.c
+++ src/01_12/commas_challenge.c
@@ -1,11 +1,56 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	int values[10] = {
-		123, 1899, 48266, 123456, 9876543,
-		10100100, 5, 500000, 99000111, 83
-	};
+			123, 1899, 48266, 123456, 9876543,
+			10100100, 5, 500000, 99000111, 83};
 
-	return(0);
+	int temp, j, idx;
+	for (int i = 0; i < 10; i++)
+	{
+		char numStr[32], out[32];
+
+		sprintf(numStr, "%d", values[i]);
+		int count = strlen(numStr);
+		int originalCount = count;
+		j = idx = 0;
+
+		do
+		{
+			if (count % 3 == 0)
+			{
+				for (int k = 0; k < 3; k++)
+					out[j++] = numStr[idx++];
+				if (count > 3)
+					out[j++] = ',';
+				count -= 3;
+			}
+			else if (count % 3 == 2)
+			{
+				for (int k = 0; k < 2; k++)
+					out[j++] = numStr[idx++];
+				if (count > 3)
+					out[j++] = ',';
+				count -= 2;
+			}
+			else if (count % 3 == 1)
+			{
+				out[j++] = numStr[idx++];
+				if (count > 3)
+					out[j++] = ',';
+				count--;
+			}
+
+			if (idx == originalCount)
+			{
+				out[j] = 0;
+				break;
+			}
+		} while (1);
+
+		printf("%12d    --> %12s\n", values[i], out);
+	}
+	return (0);
 }
--- src/01_12/commas_solution.c
+++ src/01_12/commas_solution.c
@@ -5,50 +5,49 @@
 void commify(char *s, int v)
 {
 	char temp[15];
-	int len,c,t;
+	int len, c, t;
 
 	/* convert value to string */
-	sprintf(temp,"%d",v);
+	sprintf(temp, "%d", v);
 
 	/* duplicate the string, but with commas */
 	len = strlen(temp);
-	c = t = 0;				/* index variables */
-	while(temp[t]!='\0')
+	c = t = 0; /* index variables */
+	while (temp[t] != '\0')
 	{
-		if( len % 3 )		/* comma every three characters */
+		if (len % 3) /* comma every three characters */
 		{
-			*(s+c) = temp[t];		/* no comma, so copy */
+			*(s + c) = temp[t]; /* no comma, so copy */
 		}
 		else
 		{
-			if( t != 0 )			/* avoid a trailing comma */
+			if (t != 0) /* avoid a trailing comma */
 			{
-				*(s+c) = ',';		/* add the comma */
+				*(s + c) = ','; /* add the comma */
 				c++;
 			}
-			*(s+c) = temp[t];		/* copy the following character */
+			*(s + c) = temp[t]; /* copy the following character */
 		}
 		c++;
 		t++;
 		len--;
 	}
-	*(s+c) = '\0';			/* cap the string */
+	*(s + c) = '\0'; /* cap the string */
 }
 
 int main()
 {
 	int values[10] = {
-		123, 1899, 48266, 123456, 9876543,
-		10100100, 5, 500000, 99000111, 83
-	};
+			123, 1899, 48266, 123456, 9876543,
+			10100100, 5, 500000, 99000111, 83};
 	char withcomma[15];
 	int x;
 
-	for( x=0; x<10; x++ )
+	for (x = 0; x < 10; x++)
 	{
-		commify(withcomma,values[x]);
+		commify(withcomma, values[x]);
 		printf("%10d:%15s\n", values[x], withcomma);
 	}
 
-	return(0);
+	return (0);
 }
