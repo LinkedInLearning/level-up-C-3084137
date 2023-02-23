475c2e1c0c6ac46a0466ea9c66adafff5ca07995
shantu03 <hulwanshantanu@gmail.com>
2023-02-23 10:11:01 +0530

    Completed

--- src/01_06/chop_challenge.c
+++ src/01_06/chop_challenge.c
@@ -3,24 +3,54 @@
 
 char *left(char *s,int len)
 {
+	int i;
+	char *hey;
+	hey=malloc(sizeof(char)*len+1);
+	for(i=0;i<len;i++)
+	{
+		*(hey+i)=*(s+i);
+	}
+	*(hey+i)='\0';
+	return(hey);
 }
 
 char *right(char *s,int len)
 {
+	int z,y=len;
+	char *yes;
+	yes=malloc(sizeof(char)*len+1);
+	for(z=0;z<=len;z++,y++)
+	{
+		*(yes+z)=*(s+y);
+	}
+	*(yes+z)='\0';
+	return(yes);
+	
 }
 
 char *mid(char *s,int offset, int len)
 {
+	int q,w;
+	char *mam;
+	mam=malloc(sizeof(char)*len+1);
+	for(q=0;q<=len;q++)
+	{
+		*(mam+q)=*(s+offset+q);
+		if(*(mam+q)=='\0')
+		break;
+	}
+	*(mam+q)='\0';
+	return(mam);
 }
 
 int main()
 {
-	char string[] = "Once upon a time, there was a string";
+	char string[] = "Once_upon_a_time,_there_was_a_string";
 
 	printf("Original string: %s\n",string);
 	printf("Left %d characters: %s\n",16,left(string,16));
 	printf("Right %d characters: %s\n",18,right(string,18));
-	printf("Middle %d characters: %s\n",11,mid(string,13,11));
+	printf("Middle %d characters: %s\n",11,mid(string,12,11));
 
 	return(0);
 }
